9c295db057e11aea0b2559cc9e8eecaede0126d2
JoseCarrillo19 <josecarloscarrillo@unicesar.edu.co>
2022-03-19 14:40:37 -0500

    Implementando clase, modelo, servicos de Email

---
+++ CleanArchitecture.Application/Contracts/Persistence/ILibroRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Application.Contracts.Persistence
+{
+    public interface ILibroRepository : IAsyncRepository<Libro>
+    {
+        Task<Libro> GetLibroByNombre(string nombreLibro);
+        Task<IEnumerable<Libro>> GetLibroByUserName(string userName);
+    }
+}
